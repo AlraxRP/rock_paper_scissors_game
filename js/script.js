79ce01a66eb7b24d5081f6ba5db384ee5f725941
Alexis <alexisrp_4@hotmail.com>
2025-11-12 18:42:33 -0600

    Write function to get the computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+function getComputerChoice()
+{
+    let randNum;
+    let compChoice;
+
+    randNum = Math.floor((Math.random()*3)+1);
+
+    if(randNum === 1)
+    {
+        compChoice = "Rock";
+    }
+    else if(randNum === 2)
+    {
+        compChoice = "Paper";
+    }
+    else
+    {
+        compChoice = "Scissors";
+    }
+
+    return compChoice;
+}
+
+console.log(getComputerChoice());
